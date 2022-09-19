9125afadd2c2a48ac51a20653c576d02da976d25
Several_notebook <mikha.alkhimovich@mail.ru>
2022-09-19 20:14:20 +0700

    main: finish project structure

---
+++ src/doubly_linked_lists.c
@@ -0,0 +1,2 @@
+#include "doubly_linked_lists.h"
+
---
+++ src/doubly_linked_lists.h
@@ -0,0 +1,7 @@
+#ifndef SRC_DOUBLY_LINKED_LISTS_H_
+#define SRC_DOUBLY_LINKED_LISTS_H_
+
+#include <iostream>
+#include <stdlib.h>
+
+#endif  // SRC_DOUBLY_LINKED_LISTS_H_
