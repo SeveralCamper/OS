c170042a7b9b48d3ebc0d4b23d6163cfb042f1c0
Several_notebook <mikha.alkhimovich@mail.ru>
2022-09-20 01:00:29 +0700

    main: finish pop_back

--- src/doubly_linked_lists.c
+++ src/doubly_linked_lists.c
@@ -57,6 +57,7 @@ void push_back(list_t *list, char *name_new, int *grades_new, int name_size, int
         if (list->head == NULL) {
             list->head = list->tail = tmp;
         } else {
+            list->tail->next = tmp;
             list->tail = tmp;
         }
         list->size++;
@@ -64,25 +65,44 @@ void push_back(list_t *list, char *name_new, int *grades_new, int name_size, int
 }
 
 void pop_back(list_t *list) {
-
+    if (list->head == NULL) {
+        printf("Error: List is also empty!\n");
+    } else {
+        node_t *tmp = (node_t*)malloc(sizeof(node_t));
+        if (tmp == NULL) {
+            printf("Error: Bad allocation!\n");
+        } else {
+            tmp = list->tail;
+            list->tail = list->tail->prev;
+            if (list->tail) {
+                list->tail->next = NULL;
+            }
+            if (tmp == list->head) {
+                list->head = NULL;
+            }
+            free(tmp);
+        }
+        list->size--;
+    }
 }
 
 void print_list(list_t *list, int grades_size) {
     if (list == NULL) {
         printf("List is empty!\n");
     } else {
-        list_t *p;
-        p = list;
-        while(p->head != NULL) {
-            printf("%s\n", p->head->name);
+        node_t *p = list->head;
+        int count = 0;
+        while(p != NULL) {
+            printf("%s\n", p->name);
             for (int i = 0; i < grades_size; i++) {
                 if (i == grades_size - 1) {
-                    printf("%d\n", p->head->grades[i]);
+                    printf("%d\n", p->grades[i]);
                 } else {
-                    printf("%d ", p->head->grades[i]);
+                    printf("%d ", p->grades[i]);
                 }
             }
-            p->head = p->head->next;
+            p = p->next;
+            count++;
         }
     }
 } 
@@ -91,13 +111,22 @@ int main() {
     list_t *list = create_list();
     int arr[10] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
     int count = 0;
-    for (int i = 0; i < 1000; i++) {
+    for (int i = 0; i < 10; i++) {
         push_back(list, "ABCDE", arr, 5, 10);
+        printf("\n");
+        print_list(list, 10);
+        printf("\n");
         for (int j = 0; j < 10; j++) {
             arr[j] = count + i;
             count++;
         }
+    }  
+
+    for (int i = 0; i < 10; i++) {
+        pop_back(list);
+        printf("\n");
         print_list(list, 10);
+        printf("\n");
     }
     return 0;
 }
