7cdeef39c88271783bbb0ab2c84564611710b8d9
Several_notebook <mikha.alkhimovich@mail.ru>
2022-10-04 03:17:30 +0700

    main: finish 4-nd key

--- src/4/systype.c
+++ src/4/systype.c
@@ -5,36 +5,54 @@
 #define ERROR_CREATE_THREAD -11
 #define ERROR_JOIN_THREAD -12
 
-char *enter_PID_process(char *first, int pid) {
-	char tmp;
-	char *resault = NULL;
-	int i = 0;
-	
-	resault = realloc(resault, 1 * sizeof(char));
+char *enter_PID_process(int pid) {
+	int j = 0, i = 6;
+	char *comand = NULL;
+	comand = realloc(comand, (i) * sizeof(char));
+	comand[0] = 'p';
+	comand[1] = 's';
+	comand[2] = ' ';
+	comand[3] = '-';
+ 	comand[4] = 'p';
+	comand[5] = ' ';
+	int *res_arr = NULL;
 	while(pid != 0) {
-		resault = realloc(resault, 1 * sizeof(char));
-		tmp = (char) (pid % 10) - '0';
-		resault[i] = tmp;
+		res_arr = realloc(res_arr, (j) * sizeof(int));
+		res_arr[j] = pid % 10;
+		pid = pid / 10;
+		j++;
 	}
 
-	resault = strcat(first, resault);
-	printf("%s\n", resault);
+	for (int k = j - 1; k >= 0; k--) {
+		comand = realloc(comand, (i) * sizeof(char));
+		comand[i] = res_arr[k] + '0';
+		i++;
+
+	}
+
+	for (int j = 0; j < i; j++) {
+		printf("%c\n", comand[j]);
+	}
+
+	printf("DDDD %s\n", comand);
 	
-	return resault;
+	return comand;
 }
 
 void API_contract() {
 	char c;
 	int pid = 0, API_flag = 0;
+	pid_t pid_1;
 	printf("Please, enter a task for the process:\n");
 	printf("1) Check all running process\n");
 	printf("2) Check all process in directory\n");
 	printf("3) Check procces by PID\n");
-	printf("4) \n");
+	printf("4) Generate new process\n");
 	printf("5) \n");
+	printf("6) \n");
 	printf("0) Exit.\n");
 	while (1) {
-		if (scanf("%d%c", &API_flag, &c) == 2 && (API_flag > -1 && API_flag < 7) && (c == '\n')) {
+		if (scanf("%d%c", &API_flag, &c) == 2 && (API_flag > -1 && API_flag < 7) && (c == '\n') && (c != EOF)) {
 			if (API_flag != 0) {
 				printf("\033c");
 				switch (API_flag) {
@@ -47,12 +65,42 @@ void API_contract() {
 						system("ps -T");
 						break;
 					case 3:
-						printf("Please, enter process PID: \n");
-						if (scanf("%d%c", &pid, &c) == 2 && (API_flag > 0) && (c == '\n')) {
-							system(enter_PID_process("ps -p $", pid));
+						pid = 1;
+						char c2;
+						fflush(NULL);
+						printf("\033c");
+						while (pid == 1 || pid == 2) {
+							fflush(NULL);
+							printf("Please, enter process PID or: \n");
+							printf("1) Check all running process\n");
+							printf("2) Check all process in directory\n");
+							if (scanf("%d%c", &pid, &c2) == 2 && (pid > 0) && (c2 == '\n') && (c2 != EOF)) {
+								printf("%d!\n", pid);
+								switch (pid) {
+									case 1:
+										printf("All running process:\n");
+										system("ps -e");
+										break;
+									case 2:
+										printf("All procces in directory:\n");
+										system("ps -T");
+										break;
+									default:
+										system(enter_PID_process(pid));
+										break;
+								}
+							} else {
+								printf("\033c");
+								printf("Invelid input!\n");
+								break;
+							}
+							fflush(NULL);
 						}
 						break;
 					case 4:
+						pid_1 = fork();
+						printf("Add new process:\n");
+						printf("New procces PID is %i\n", pid_1 = (int)getpid());
 						break;
 					case 5:
 						break;
@@ -64,8 +112,9 @@ void API_contract() {
 				printf("1) Check all running process\n");
 				printf("2) Check all process in directory\n");
 				printf("3) Check procces by PID\n");
-				printf("4) \n");
+				printf("4) Generate new process\n");
 				printf("5) \n");
+				printf("6) \n");
 				printf("0) Exit.\n");
 			} else {
 				printf("\033c");
@@ -73,6 +122,7 @@ void API_contract() {
 			}
 		} else {
 			printf("Invalid input!\n");
+			break;
 		}
 	}
 }
